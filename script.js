b984b23c2936507723545be6dd290c975291e530
maho1203 <mahopi1001@gmail.com>
2025-03-06 14:09:38 +0900

    change script.js

--- script.js
+++ script.js
@@ -123,14 +123,8 @@ function showResult(resultKey) {
     questionContainer.style.display = "none";
     resultContainer.style.display = "block";
 
-    // 改行ごとに <p> 要素を作成
-    resultText.innerHTML = results[resultKey]
-        .split("\n")
-        .filter(line => line.trim() !== "") // 空行を削除
-        .map(line => `<p>${line}</p>`)
-        .join("");
+    resultText.textContent = results[resultKey];
 }
-
 
 function restart() {
     currentStep = 0;
